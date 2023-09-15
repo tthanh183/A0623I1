242d49785f2512c6b62d9b8994f9d0fb93cb2977
thanh <trantanthanh29@gmail.com>
2023-09-15 09:33:01 +0700

    1.introduction

---
+++ module2/1.introduction/Greeting.java
@@ -0,0 +1,12 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
