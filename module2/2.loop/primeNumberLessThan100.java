625c179fb0d8d5a1b1ce1949a62863c1a75f1eda
thanh <trantanthanh29@gmail.com>
2023-09-15 10:38:19 +0700

    2,loop

---
+++ module2/2.loop/primeNumberLessThan100.java
@@ -0,0 +1,25 @@
+package loop;
+
+public class primeNumberLessThan100 {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 2;
+        while(number < 100 ) {
+            if(isPrime(number)) {
+                System.out.print(number + " ");
+            }
+            number ++;
+        }
+    }
+}
---
+++ module2/2.loop/showPrimeNumber.java
@@ -0,0 +1,27 @@
+package loop;
+
+public class showPrimeNumber {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        while (count < 20) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+}
